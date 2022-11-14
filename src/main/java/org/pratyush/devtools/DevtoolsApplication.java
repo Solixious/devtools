3004df47c8a8d6ce4fb088806f930570099aa208
Pratyush Choudhary <solixious@gmail.com>
2022-11-14 10:21:17 +0400

    Initial Commit

---
+++ src/main/java/org/pratyush/devtools/DevtoolsApplication.java
@@ -0,0 +1,13 @@
+package org.pratyush.devtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevtoolsApplication.class, args);
+	}
+
+}
